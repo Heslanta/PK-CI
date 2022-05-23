c680787e8aa43c107571522329aabb0b3292f6cc
Heslanta <charviawijaya01@gmail.com>
2022-05-23 22:56:05 +0800

    disabled textarea

--- app/Views/konsul/detail.php
+++ app/Views/konsul/detail.php
@@ -16,8 +16,8 @@
                     <h5 class="card-title"> Konsultasi ke - <?= $konsul['konsul_ke']; ?></h5>
                     <p class="card-text">Tanggal : <?= $konsul['hari_tanggal']; ?></p>
                     <p class="card-text"> Tujuan : <?= $konsul['tujuan']; ?></p>
-                    <p class="card-text">Hasil Konsultasi : <?php echo "<table><tbody><tr><td><textarea rows=\"10\" cols=\"130\" >" . $konsul['hasil_konsul'] . "</textarea></td></tr></tbody></table>"; ?> </p>
-                    <p class="card-text">Catatan Konsultasi : <?php echo "<table><tbody><tr><td><textarea rows=\"10\" cols=\"130\" >" . $konsul['catatan_konsul'] . "</textarea></td></tr></tbody></table>"; ?> </p>
+                    <p class="card-text">Hasil Konsultasi : <?php echo "<table><tbody><tr><td><textarea disabled rows=\"10\" cols=\"130\" >" . $konsul['hasil_konsul'] . "</textarea></td></tr></tbody></table>"; ?> </p>
+                    <p class="card-text">Catatan Konsultasi : <?php echo "<table><tbody><tr><td><textarea disabled rows=\"10\" cols=\"130\" >" . $konsul['catatan_konsul'] . "</textarea></td></tr></tbody></table>"; ?> </p>
                     <a href="/konsul/edit/<?= $konsul['id_konsul']; ?>" class="btn btn-primary">Edit</a>
 
                     <button onclick="history.back()" class="btn btn-warning">Kembali</button>
