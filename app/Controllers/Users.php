e38ec888af975321941f522ba07c2f85a95548a8
Heslanta <charviawijaya01@gmail.com>
2022-05-26 15:26:34 +0800

    edit dikit

--- app/Controllers/Users.php
+++ app/Controllers/Users.php
@@ -84,4 +84,69 @@ class Users extends BaseController
         session()->setFlashdata('pesan', 'Data berhasil ditambahkan.');
         return redirect()->to(base_url() . '/users');
     }
+
+    public function login()
+    {
+        $data = [];
+
+        if ($this->request->getMethod() == 'post') {
+
+            $rules = [
+                'email' => 'required|min_length[6]|max_length[50]|valid_email',
+                'password' => 'required|min_length[8]|max_length[255]|validateUser[email,password]',
+            ];
+
+            $errors = [
+                'password' => [
+                    'validateUser' => "Email or Password didn't match",
+                ],
+            ];
+
+            if (!$this->validate($rules, $errors)) {
+
+                return view('login', [
+                    "validation" => $this->validator,
+                ]);
+            } else {
+                $model = new UsersModel();
+
+                $user = $model->where('email', $this->request->getVar('email'))
+                    ->first();
+
+                // Stroing session values
+                $this->setUserSession($user);
+
+                // Redirecting to dashboard after login
+                if ($user['role'] == "admin") {
+
+                    return redirect()->to(base_url('admin'));
+                } elseif ($user['role'] == "editor") {
+
+                    return redirect()->to(base_url('editor'));
+                }
+            }
+        }
+        return view('login');
+    }
+
+    private function setUserSession($user)
+    {
+        $data = [
+            'id' => $user['id'],
+            'name' => $user['name'],
+            'phone_no' => $user['phone_no'],
+            'email' => $user['email'],
+            'isLoggedIn' => true,
+            "role" => $user['role'],
+        ];
+
+        session()->set($data);
+        return true;
+    }
+
+    public function logout()
+    {
+        session()->destroy();
+        return redirect()->to('login');
+    }
 }
