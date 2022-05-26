bd38d4fabc258f4565e7d492ad66e0bac9a66aea
Heslanta <charviawijaya01@gmail.com>
2022-05-26 08:35:57 +0800

    tambah header use

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -8,6 +8,8 @@ use CodeIgniter\Filters\DebugToolbar;
 use CodeIgniter\Filters\Honeypot;
 use CodeIgniter\Filters\InvalidChars;
 use CodeIgniter\Filters\SecureHeaders;
+use App\Filters\Auth;
+use App\Filters\Noauth;
 
 class Filters extends BaseConfig
 {
