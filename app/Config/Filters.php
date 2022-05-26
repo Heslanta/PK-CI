0aa5c35852fc5a00e0ca422d955ede97461ef99a
Heslanta <charviawijaya01@gmail.com>
2022-05-26 08:35:09 +0800

    nambah $alias

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -23,6 +23,8 @@ class Filters extends BaseConfig
         'honeypot'      => Honeypot::class,
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
+        "auth" => Auth::class,
+        "noauth" => Noauth::class,
     ];
 
     /**
