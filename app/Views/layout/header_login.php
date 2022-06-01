aed5197befccaba45e8f1498b64f59040ee0be69
Heslanta <charviawijaya01@gmail.com>
2022-06-01 13:44:03 +0800

    tambah dan buat headerlogin

---
+++ app/Views/layout/header_login.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400">
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Montserrat'>
+    <link rel="stylesheet" href="<?= base_url(); ?>/css/style.css">
+    <link rel="stylesheet" href="<?= base_url(); ?>/css/main.css">
+    <title>Login HLP</title>
+</head>
+
+<body>
+    <?= $this->renderSection('content'); ?>
