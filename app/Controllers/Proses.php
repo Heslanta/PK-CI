7b01ab0fd45748242e8ff182534c97d80a6d73d3
Charvia <90585985+Heslanta@users.noreply.github.com>
2022-12-16 20:41:52 +0800

    nambah controller proses

---
+++ app/Controllers/Proses.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\JadwalModel;
+use App\Models\ProsesModel;
+
+class Proses extends BaseController
+{
+    protected $prosesModel;
+    public function __construct()
+    {
+        $this->prosesModel = new ProsesModel();
+        $this->jadwalModel = new JadwalModel();
+    }
+
+    public function terima()
+    {
+        // update di proses jadwal klien
+        $model = new ProsesModel();
+        $id = $this->request->getPost('id_jadwal');
+        $proses = 'diterima';
+        $data = array(
+            'proses'      => $proses
+        );
+        $model->updateProsesJadwal($data, $id);
+
+        // sesudah diterima, akan disimpan pada tabel jadwal sehingga muncul pada admin dan pegawai
+        $this->jadwalModel->save([
+            'nama'        => $this->request->getPost('nama'),
+            'id_user'        => $this->request->getPost('id_user'),
+            'tujuan_jdw'  => $this->request->getPost('tujuan_jdw'),
+            'tanggal'     => $this->request->getPost('tanggal'),
+            'status'      => $this->request->getPost('status'),
+            'proses'      => $proses,
+        ]);
+        session()->setFlashdata('pesan', 'Data berhasil ditambahkan');
+
+        return redirect()->to('pages/index');
+    }
+    public function tolak()
+    {
+        // update di proses jadwal klien
+        $model = new ProsesModel();
+        $id = $this->request->getPost('id_jadwal');
+        $proses = 'ditolak';
+        $alasan = $this->request->getPost('alasan');
+        if (empty($alasan)) {
+            $alasan = 'Tidak ada alasan';
+        }
+        $data = array(
+            'proses'      => $proses,
+            'alasan'      => $alasan
+        );
+        $model->updateProsesJadwal($data, $id);
+
+        // sesudah diterima, akan disimpan pada tabel jadwal sehingga muncul pada admin dan pegawai
+
+        session()->setFlashdata('pesan', 'Data berhasil diperbaharui');
+
+        return redirect()->to('pages/index');
+    }
+
+
+
+
+    // function untuk menyimpan jadwal pada page klien
+    public function saveklien()
+    {
+        $model = new ProsesModel();
+        $proses = 'menunggu';
+        $this->prosesModel->save([
+            'nama'        => $this->request->getPost('nama'),
+            'tujuan_jdw'  => $this->request->getPost('tujuan_jdw'),
+            'tanggal'     => $this->request->getPost('tanggal'),
+            'status'      => $this->request->getPost('status'),
+            'id_user'      => $this->request->getPost('id'),
+            'proses'      => $proses,
+        ]);
+        // dd($test);
+
+        session()->setFlashdata('pesan', 'Data berhasil ditambahkan');
+
+        return redirect()->to('pages/klienberanda');
+    }
+    // public function edit($id)
+    // {
+    //     $data = [
+    //         'title' => 'Edit Pengguna',
+    //         'validation' => \Config\Services::validation(),
+    //         'user' => $this->usersModel->getUser(($id)),
+    //         'css' => 'change-client-data-style'
+    //     ];
+
+    //     return view('users/edit', $data);
+    // }
+
+
+
+    // function untuk meupdate jadwal pada beranda klien
+    public function upjadwalklien()
+    {
+        $model = new ProsesModel();
+        $id = $this->request->getPost('id_jadwal');
+        $data = array(
+            'tujuan_jdw'  => $this->request->getPost('tujuan_jdw'),
+            'tanggal'     => $this->request->getPost('tanggal'),
+            'status'      => $this->request->getPost('status'),
+        );
+        $model->updateProsesJadwal($data, $id);
+        session()->setFlashdata('pesan', 'Data berhasil diperbaharui');
+
+        return redirect()->to('pages/klienberanda');
+    }
+
+
+
+    public function delete()
+    {
+        $model = new ProsesModel();
+        $id = $this->request->getPost('id_jadwal');
+        $model->deleteProsesJadwal($id);
+        return redirect()->to('pages/klienberanda');
+        session()->setFlashdata('pesan-hapus', 'Data berhasil dihapus');
+    }
+}
